0f2e6fbe9f758efc55bcdbd44b6d887093c45f09
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2016-06-28 12:52:58 -0400

    fix formatting

--- web/server.py
+++ web/server.py
@@ -6,7 +6,7 @@ import sqlite3
 import command
 import etcd
 
-#set up database connection
+# set up database connection
 conn = sqlite3.connect('../master/vnfs.db')
 cur = conn.cursor()
 
@@ -17,45 +17,49 @@ etcdcli = etcd.Client()
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
-    
 def root():
 	return app.send_static_file('index.html')
-    
+
+
 @app.route('/js/monitor.js')
 def send_monitor():
 	return app.send_static_file('js/monitor.js')
-    
+
+
 @app.route('/js/model.js')
 def send_model():
 	return app.send_static_file('js/model.js')
-	
-    
+
+
 @app.route('/css/monitor.css')
 def send_css():
 	return app.send_static_file('css/monitor.css')
 
-    
+
 @app.route('/getVNF', methods=['GET'])
 def send_VNF():
-	#cur.execute('select * from VNF')
-	#results = cur.fetchall()
+	# cur.execute('select * from VNF')
+	# results = cur.fetchall()
 	results = []
 	r = etcdcli.read('/VNF', recursive=True, sorted=True)
 	for child in r.children:
 		results.append(json.loads(child.value))
 	return jsonify(results)
 
+
 @app.route('/getHost', methods=['GET'])
 def send_Host():
-	#cur.execute('select * from Host')
-	#results = cur.fetchall()
+	# cur.execute('select * from Host')
+	# results = cur.fetchall()
 	results = []
 	r = etcdcli.read('/Host', recursive=True, sorted=True)
 	for child in r.children:
 		results.append(json.loads(child.value))
 	return jsonify(results)
-	
+
+
 @app.route('/getChain', methods=['GET'])
 def send_Chain():
 	results = []
@@ -63,25 +67,26 @@ def send_Chain():
 	for child in r.children:
 		results.append(json.loads(child.value))
 	return jsonify(results)
-	
+
+
 @app.route('/request', methods=['POST'])
 def reply():
 	msg = request.get_json()
 	print(msg)
 	if (msg['action'] == 'start'):
-		cmd.start(msg['Host'],msg['ID'])
+		cmd.start(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'stop'):
-			cmd.stop(msg['Host'],msg['ID'])
+			cmd.stop(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'restart'):
-		cmd.restart(msg['Host'],msg['ID'])
+		cmd.restart(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'destroy'):
-		cmd.destroy(msg['Host'],msg['ID'])
+		cmd.destroy(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'pause'):
-		cmd.pause(msg['Host'],msg['ID'])
+		cmd.pause(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'unpause'):
-		cmd.unpause(msg['Host'],msg['ID'])
+		cmd.unpause(msg['Host'], msg['ID'])
 	elif(msg['action'] == 'deploy'):
-		cmd.deploy(msg['Host'],msg['username'],msg['image'],msg['vnfname'])
+		cmd.deploy(msg['Host'], msg['username'], msg['image'], msg['vnfname'])
     elif(msg['create_chain'] == 'deploy_chain'):
         cmd.create_chain()
 	return jsonify({'result' : 'command received'})
