fd970063f6921b8545627f634ca54973ac7ad47a
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2016-06-29 00:48:06 -0400

    Add rollback in chain provisioning

--- minion/container_driver.py
+++ minion/container_driver.py
@@ -264,6 +264,19 @@ class ContainerDriver():
         with self._error_handling(errors.VNFDestroyError):
             dcx.remove_container(container=vnf_fullname, force=force)
 
+    def unlink_container_netns(self, vnf_name):
+        """
+        Deletes the symbolic link to the container's network namespace.
+
+        @param vnf_name Name of the VNF
+        """
+        vnf_container_pid = str(self.get_container_pid(vnf_name))
+        if os.path.exists('/var/run/netns/' + vnf_container_pid):
+            bash_command = "sudo rm /var/run/netns/" + vnf_container_pid
+            (return_code, output, errput) = execute_bash_command(bash_command)
+            if return_code <> 0:
+                raise Exception(return_code, errput)
+
     def symlink_container_netns(self, vnf_name):
         """
         Creates a symbolik link of the container's namespace to
--- minion/provisioning_agent.py
+++ minion/provisioning_agent.py
@@ -3,6 +3,7 @@ from bash_wrapper import execute_bash_command
 from random import randint
 from veth_driver import VethDriver
 from ovs_driver import OVSDriver
+from rollbackcontext import RollbackContext
 
 
 class ProvisioningAgent():
@@ -11,17 +12,28 @@ class ProvisioningAgent():
         self.__container_handle = ContainerDriver()
         self.__default_ovs_bridge = ovs_bridge_name
 
-    def provision_single_vnf(self, vnf_config):
+    def provision_single_vnf(self, vnf_config, chain_rollback):
+        # Deploy the container.
         self.__container_handle.deploy(
             user="sr2chowd",
             image_name=vnf_config['vnf_type'],
             vnf_name=vnf_config['container_name'])
         container_name = "sr2chowd-" + vnf_config['container_name']
-        self.__container_handle.start(vnf_name=container_name)
+        chain_rollback.push(self.__container_handle.destroy,
+                self.__container_handle, container_name)
 
+        # Start the container 
+        self.__container_handle.start(vnf_name=container_name)
+        chain_rollback.push(self.__container_handle.stop, self._container_handle,
+                container_name)
+
+        # Retrieve container pid and create symlink to netns under
+        # /var/run/netns
         cont_pid = str(self.__container_handle.get_container_pid(container_name))
         print "Container " + container_name + " started with pid " + cont_pid
         self.__container_handle.symlink_container_netns(container_name)
+        chain_rollback.push(self.__container_handle.symlink_container_netns,
+                self.__container_handle, container_name)
 
         for net_config in vnf_config['net_ifs']:
             print net_config
@@ -29,19 +41,27 @@ class ProvisioningAgent():
                     self.__default_ovs_bridge)
             print "Creating veth pair (" + veth_cn + ", " + veth_vs + ")\n" 
             VethDriver.create_veth_pair(veth_cn, veth_vs)
+            chain_rollback.push(VethDriver.delete_veth_interface, veth_cn)
+            chain_rollback.push(VethDriver.delete_veth_interface, veth_vs)
+
             VethDriver.move_veth_interface_to_netns(veth_cn, netns = cont_pid)
             VethDriver.update_mtu_at_veth_interface(veth_cn, 1436, netns = cont_pid)
             VethDriver.enable_veth_interface(veth_cn, netns = cont_pid)
+
             VethDriver.assign_ip_to_veth_interface(veth_cn, net_config['ip_address'], netns =
                     cont_pid)
+            chain_rollback.push(VethDriver.delete_ip_from_veth_interface,
+                    net_config['ip_address'], netns = cont_pid)
+
             VethDriver.enable_veth_interface(veth_cn, netns = cont_pid)
 
-
     def provision_local_chain(self, chain_config):
-        for vnf_config in chain_config:
-            print "####################"
-            print vnf_config
-            self.provision_single_vnf(vnf_config)
+        with RollbackContext() as chain_rollback:
+            for vnf_config in chain_config:
+                print "####################"
+                print vnf_config
+                self.provision_single_vnf(vnf_config)
+            chain_rollback.commitAll()
 
     def generate_unique_veth_endpoints(self, container_name, ovs_bridge_name):
         veth_endpoint_a = ""
