b2aba38198a185705929a8eac614deee91e350cf
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2016-08-16 13:10:21 -0400

    Add container_driver test

--- test/test_container.py
+++ test/test_container.py
@@ -1,47 +1,67 @@
-import sys, os
+import sys
+import os
 sys.path.append(os.getcwd() + "/..")
 from minion import container_driver
 
-mon = container_driver.ContainerDriver("minion.mock_docker")
+cdriver = container_driver.ContainerDriver("minion.mock_docker")
 
 def test_get_containers():
-	assert(mon.get_containers() != None)
+    assert(cdriver.get_containers() != None)
+    assert(len(cdriver.get_containers(True)) == 2)
+    assert(len(cdriver.get_containers(False)) == 1)
+
+def test_is_empty():
+    assert(cdriver._is_empty("") == True)
+    assert(cdriver._is_empty("not-empty") == False)
+    assert(cdriver._is_empty("0") == False)
 
 def test_get_id():
-	assert(mon.get_id('nfuser-new-firewall') == 'd998116c32849114eae8152c060fcd4cc12989e9b3a1c71e6172d42fa43a9331')
-	
+    assert(cdriver.get_id('nfuser-new-firewall') ==
+           'd998116c32849114eae8152c060fcd4cc12989e9b3a1c71e6172d42fa43a9331')
+
 def test_get_container_pid():
-	assert(mon.get_container_pid('nfuser-new-firewall') == 3790)
-	
+    assert(cdriver.get_container_pid('nfuser-new-firewall') == 3790)
+
+
 def test_get_ip():
-	assert(mon.get_ip('nfuser-new-firewall') == '172.17.0.2')
+    assert(cdriver.get_ip('nfuser-new-firewall') == '172.17.0.2')
+
 
 def test_execute_in_guest():
-    assert(mon.execute_in_guest('nfuser-new-firewall','ls') == None)
-    
+    assert(cdriver.execute_in_guest('nfuser-new-firewall', 'ls') == None)
+
+
 def test_guest_status():
-    assert(mon.guest_status('nfuser-new-firewall') == 'running')
-    
+    assert(cdriver.guest_status('nfuser-new-firewall') == 'running')
+
+
 def test_start():
-    mon.start('whatever')
-    
+    cdriver.start('whatever')
+
+
 def test_restart():
-    mon.restart('whatever')
+    cdriver.restart('whatever')
+
 
 def test_stop():
-    mon.stop('whatever')
+    cdriver.stop('whatever')
+
 
 def test_pause():
-    mon.pause('whatever')
-    
+    cdriver.pause('whatever')
+
+
 def test_unpause():
-    mon.unpause('whatever')
-    
+    cdriver.unpause('whatever')
+
+
 def test_deploy():
-    assert(mon.deploy('a','b','c') == '0000000000')
-    
+    assert(cdriver.deploy('a', 'b', 'c') == '0000000000')
+
+
 def test_destroy():
-    mon.destroy('whatever')
-    
+    cdriver.destroy('whatever')
+
+
 def test_images():
-    assert(mon.images() != None)
+    assert(cdriver.images() != None)
