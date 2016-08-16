afb957be0de0f8b21402317436d1a5dc943213c6
Shihabur Rahman Chowdhury <sr2chowdhury@uwaterloo.ca>
2016-08-16 18:38:15 -0400

    Add a mock for bash command wrapper

---
+++ minion/mock_bash_wrapper.py
@@ -0,0 +1,55 @@
+from mock_ovs_driver import MockOVSDriver
+import commandwrapper
+
+odriver = MockOVSDriver()
+
+def __execute_pipe_command(pipe_command, pipe_stdin):
+    pipe_cmd = commandwrapper.WrapCommand(pipe_command)
+    pipe_cmd.stdin = pipe_stdin
+    pipe_cmd.start()
+    pipe_cmd.join()
+    return pipe_cmd.results
+
+def execute_bash_command(bash_command):
+    ret = 0 
+    out = "" 
+    err = ""
+    tokens = bash_command.split(" ")
+    command = tokens[0] 
+    if command == "sudo":
+        tokens = tokens[1:]
+        command = tokens[0]
+    tokens = tokens[1:]
+    if command == "ovs-vsctl":
+        subcommand = tokens[0]
+        tokens = tokens[1:]
+        if subcommand == "add-br":
+            args = tokens[0]
+            ret, out, err = odriver.create_bridge(args)
+        elif subcommand == "del-br":
+            args = tokens[0]
+            ret, out, err = odriver.delete_bridge(args)
+            print args, ret, out, err
+        elif subcommand == "show":
+            pipe_command = ""
+            if len(tokens) > 0:
+                pipe_command = " ".join(tokens[1:])
+            bridges = "\n".join(odriver.get_bridges())
+            out, err = __execute_pipe_command(pipe_command, bridges)
+            ret = 0
+        elif subcommand == "add-port":
+            bridge, port = tokens[0], tokens[1]
+            ret, out, err = odriver.attach_interface_to_ovs(bridge, port)
+        elif subcommand == "del-port":
+            bridge, port = tokens[0], tokens[1]
+            ret, out, err = odriver.detach_interface_from_ovs(bridge, port)
+        elif subcommand == "list-ports":
+            bridge = tokens[0]
+            tokens = tokens[1:]
+            pipe_command = ""
+            if len(tokens) > 0:
+                pipe_command = " ".join(tokens[1:])
+            ports = "\n".join(odriver.get_ports(bridge))
+            out, err = __execute_pipe_command(pipe_command, ports)
+            ret = 0
+    return (ret, out, err)
